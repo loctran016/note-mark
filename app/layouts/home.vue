4d8d9d4759947ba7fa7230befefe21345093f75a
Tran Tan Loc <locphuc016@gmail.com>
2025-08-07 15:41:49 +0700

    home dark

--- app/layouts/home.vue
+++ app/layouts/home.vue
@@ -1,4 +1,10 @@
-<script></script>
+<script setup>
+useHead({
+  bodyAttrs: {
+    class: 'dark:bg-zinc-950 text-zinc-900 dark:text-gray-100'
+  }
+})
+</script>
 <template>
   <div class="w-screen h-screen flex flex-col dark:bg-zinc-950 text-zinc-900 dark:text-gray-100">
     <NavBar></NavBar>
